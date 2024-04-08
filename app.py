c7572214cf5d094e13cf24799d9b3c43ce49eabd
EvErToN <evertonflorenciodasilva@outlook.com>
2024-04-08 13:15:46 -0300

    tentando resouver erro no sqlalchemy

--- app.py
+++ app.py
@@ -1,49 +1,52 @@
-import render
-from flask import Flask, redirect, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-db = SQLAlchemy
+
 app.config['SQLALCHEMY_DATABASE_URI'] = \
     '{SGBD}://{usuario}:{senha}@{servidor}/{database}'.format(
         SGBD='mysql+mysqlconnector',
         usuario='root',
         senha='Efds11091999.',
-        servidor='localhost',
+        servidor='Mysql@localhost:3306',
         database='cadastro'
     )
 
 db = SQLAlchemy(app)
-
-class Jogos(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    nome = db.Column(db.String(50), nullable=False)
-    categoria = db.Column(db.String(40), nullable=False)
-    console = db.Column(db.String(20), nullable=False)
-
+class cadastro(db.Model):
+    cpf = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String, primary_key=True, nullable=False)
+    cpf = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), nullable=False)
     def __repr__(self):
         return '<Name %r>' % self.name
 
 
-@app.route("/")
-def login():
-
-    return render_template('/login.html', titulo='login')
+@app.route('/')
+def inicio():
+    return render_template('login.html')
 @app.route('/cadastro')
 def cadastro():
-    return render_template('/cadastro.html', titulo="cadastro")
-@app.route('/cadastro', methods=['POST,'])
-def cadastro():
-    nome = request.form['name']
-    senha = request.form['password']
-    data = request.form['birthday']
-    jogos = Jogos.query.filter_by(nome=nome).first()
+    return render_template('/cadastro.html')
+@app.route('/criar', methods=['POST',])
+def criar():
+    name = request.form['name']
+    password = request.form['password']
+    email = request.form['email']
+    birthday = request.form['birthday']
+    grupo = cadastro.query.filter_by(nome=cadastro).first()
+    if grupo:
+        print('ja existe esse produto')
+    return redirect(url_for('login.html'))
+    novo_jogo = cadastro(nome=nome, categoria=categoria, console=console)
+    db.session.add(novo_produto)
+    db.session.commit()
+    return redirect(url_for('login,html'))
 @app.route('/lista')
-def lista_de_produtos():
-    if login is True:
-        return render_template('/lista.html', titulo='lista')
-    else:
-        return redirect('/cadastro')
+def lista():
+   return redirect('/')
+
+
 app.run(debug=True)
 
 
