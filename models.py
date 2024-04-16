509bb4af0cc5c61a71c8db70b3cd47bed99c3c9a
EvErToN <evertonflorenciodasilva@outlook.com>
2024-04-16 02:34:45 -0300

    consegui resolver os erros que estava tendo assim como criei o caminho para o cadastro apartir do login e impedir de ter acesso direto a tebela sem se fazer o login

--- models.py
+++ models.py
@@ -5,7 +5,7 @@ from app import db
 class info(db.Model):
         nome = db.Column(db.String(50), nullable=False)
         cpf = db.Column(db.Integer, primary_key=True)
-        celular = db.Column(db.String(120), unique=True, nullable=False)
-        password = db.Column(db.String(120), unique=True, nullable=False)
+        email = db.Column(db.String(120), unique=True, nullable=False)
+        data_de_nascimento = db.Column(db.Date, unique=True, nullable=False)
 def __repr__(self):
-    return '<Name %r>' % self.nome
+    return '<nome %r>' % self.nome
--- views.py
+++ views.py
@@ -1,48 +1,50 @@
 from flask import render_template, request, redirect, session, flash, url_for
 
-import models
-from app import app
+from models import info
+from app import app, db
+
 
 @app.route('/')
 def login():
     proxima = request.args.get('proxima')
     return render_template('login.html', proxima=proxima)
 
-app.route('/valida')
+@app.route('/valida', methods=['POST',])
 def validacao():
-    usuario = models.info.query.filter_by(nickname=request.form['nome']).first()
+    usuario = info.query.filter_by(nickname=request.form['nome']).first()
     if usuario:
         if request.form['senha'] == usuario.senha:
             session['usuario_logado'] = usuario.nickname
             flash(usuario.nickname+'logado com sucesso')
-            proxima_pagina = request.form['proxima']
+            proxima_pagina = request.form['cadastro']
             return redirect(proxima_pagina)
-
-@app.route('/lista')
-def lista():
-    return render_template('lista.html')
 
 @app.route('/cadastro')
 def cadastro():
     return render_template('cadastro.html')
-@app.route('/criar', methods = ['POST',])
+@app.route('/criar', methods=['POST',])
 def cadastrar():
-   nome = request.form['nome']
-   cpf = request.form['password']
-   email = request.form['email']
-   dado = models.info.query.filter_by(nome=nome).first()
-   if info:
-       flash('Jogo já existente!')
-       return redirect(url_for('cadastro'))
-   novo_jogo = info(nome=nome, categoria=categoria, console=console)
-   db.session.add(novo_jogo)
-   db.session.commit()
+    nome = request.form['nome']
+    cpf = request.form['cpf']
+    email = request.form['email']
+    data_de_nascimento = request.form['birthday']
 
-   return redirect(url_for('index'))
+    dado = info.query.filter_by(cpf=cpf).first()
+    if dado:
+        flash('Jogo já existente!')
+        return redirect(url_for('cadastro'))
+
+    novo_jogo = info(nome=nome, cpf=cpf, email=email, data_de_nascimento=data_de_nascimento)
+    db.session.add(novo_jogo)
+    db.session.commit()
+
+    return redirect(url_for('login'))
 @app.route('/tabela')
 def tabela():
-    return render_template('tabela.html',item=lista)
+    if 'usuario_logado' not in session or session['usuario_logado'] is None:
+        return redirect('/')
+    return render_template('tabela.html')
 @app.route('/table', methods = ['POST',])
 def table():
-    lista = models.info.query.order_by(models.info.cpf)
+    lista = info.query.order_by(info.cpf)
     return render_template('tabela.html', item=lista)
